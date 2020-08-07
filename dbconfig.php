5483038db75fec922868d63f6ee40b028b08b84a
Mostafa Mousavi <38524302+m-mosuavi@users.noreply.github.com>
2020-08-07 22:35:16 +0430

    Add Db Config

--- dbconfig.php
+++ dbconfig.php
@@ -7,6 +7,7 @@ define('DB_NAME', 'crud');
 
 try{
 $conn = new PDO('mysql:host=' . DB_HOST .';dbname=' . DB_NAME, DB_USER, DB_PASS);
+$conn->exec('SET NAMES utf8');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
